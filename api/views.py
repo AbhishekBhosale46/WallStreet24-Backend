16436458e855ff1d1a0077445ac039d44f4293cf
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2024-03-02 23:18:00 +0530

    fix: converted buy/sell enpoints from function based to class based

--- api/views.py
+++ api/views.py
@@ -39,118 +39,122 @@ class StockDetail(generics.RetrieveAPIView):
     serializer_class = serializers.StockDetailSerializer
 
 
-@api_view(["POST"])
-def buy_stock(request, id):
-    
-    user = request.user
-    stock = get_object_or_404(Stock, id=id)
-    portfolio = Portfolio.objects.get(user=user)
+class BuyStockApi(APIView):
 
-    buy_price = request.data.get("price", None)
-    buy_qty = request.data.get("quantity", None)
+    def post(self, request, id):
 
-    if not buy_price:
-        return Response({"detail": "Buy price not given"}, status=status.HTTP_400_BAD_REQUEST)
+        user = request.user
+        stock = get_object_or_404(Stock, id=id)
+        portfolio = Portfolio.objects.get(user=user)
 
-    if not buy_qty:
-        return Response({"detail": "Buy quantity not given"}, status=status.HTTP_400_BAD_REQUEST)
+        buy_price = request.data.get("price", None)
+        buy_qty = request.data.get("quantity", None)
 
-    buy_price = Decimal(buy_price)
-    buy_qty = int(buy_qty)
+        if not buy_price:
+            return Response({"detail": "Buy price not given"}, status=status.HTTP_400_BAD_REQUEST)
 
-    price_diff = ((buy_price-stock.current_price)/(stock.current_price))*100
-    if price_diff > 2 or price_diff < -2:
-        return Response({"detail": "Cannot buy at this price"}, status=status.HTTP_400_BAD_REQUEST)
+        if not buy_qty:
+            return Response({"detail": "Buy quantity not given"}, status=status.HTTP_400_BAD_REQUEST)
 
-    available_cash = Decimal(portfolio.cash)
-    required_cash = buy_price * buy_qty
+        buy_price = Decimal(buy_price)
+        buy_qty = int(buy_qty)
 
-    if required_cash > available_cash:
-        return Response({"detail": "You don't have enough cash to buy this stock"}, status=status.HTTP_400_BAD_REQUEST)
+        price_diff = ((buy_price-stock.current_price)/(stock.current_price))*100
+        if price_diff > 2 or price_diff < -2:
+            return Response({"detail": "Cannot buy at this price"}, status=status.HTTP_400_BAD_REQUEST)
 
-    available_cash = available_cash - required_cash
-    portfolio.cash = available_cash
-    portfolio.save()
+        available_cash = Decimal(portfolio.cash)
+        required_cash = buy_price * buy_qty
 
-    transaction = Transaction.objects.create(
-        user=user,
-        stock=stock,
-        traded_price=buy_price,
-        quantity=buy_qty,
-        transaction_type="buy"
-    )
+        if required_cash > available_cash:
+            return Response({"detail": "You don't have enough cash to buy this stock"}, status=status.HTTP_400_BAD_REQUEST)
 
-    holding = Holding.objects.create(
-        portfolio=portfolio,
-        stock=stock,
-        transaction=transaction,
-        quantity=buy_qty,
-    )
+        available_cash = available_cash - required_cash
+        portfolio.cash = available_cash
+        portfolio.save()
 
-    return Response({"detail": "Transaction completed"}, status=status.HTTP_201_CREATED)
+        transaction = Transaction.objects.create(
+            user=user,
+            stock=stock,
+            traded_price=buy_price,
+            quantity=buy_qty,
+            transaction_type="buy"
+        )
+
+        holding = Holding.objects.create(
+            portfolio=portfolio,
+            stock=stock,
+            transaction=transaction,
+            quantity=buy_qty,
+        )
+
+        return Response({"detail": "Transaction completed"}, status=status.HTTP_201_CREATED)
 
 
-@api_view(["POST"])
-def sell_stock(request, id):
+class SellStockApi(APIView):
 
-    user = request.user
-    stock = get_object_or_404(Stock, id=id)
-    portfolio = Portfolio.objects.get(user=user)
+    def post(self, request, id):
 
-    sell_price = request.data.get("price", None)
-    sell_qty = request.data.get("quantity", None)
+        user = request.user
+        stock = get_object_or_404(Stock, id=id)
+        portfolio = Portfolio.objects.get(user=user)
 
-    if not sell_price:
-        return Response({"detail": "Sell price not given"}, status=status.HTTP_400_BAD_REQUEST)
+        sell_price = request.data.get("price", None)
+        sell_qty = request.data.get("quantity", None)
 
-    if not sell_qty:
-        return Response({"detail": "Sell quantity not given"}, status=status.HTTP_400_BAD_REQUEST)
+        if not sell_price:
+            return Response({"detail": "Sell price not given"}, status=status.HTTP_400_BAD_REQUEST)
 
-    sell_price = Decimal(sell_price)
-    sell_qty = int(sell_qty)
+        if not sell_qty:
+            return Response({"detail": "Sell quantity not given"}, status=status.HTTP_400_BAD_REQUEST)
 
-    price_diff = ((sell_price-stock.current_price)/(stock.current_price))*100
-    if price_diff > 2 or price_diff < -2:
-        return Response({"detail": "Cannot sell at this price"}, status=status.HTTP_400_BAD_REQUEST)
-    
-    available_holdings = Holding.objects.filter(portfolio=portfolio, stock=stock).order_by("transaction__transaction_datetime")
+        sell_price = Decimal(sell_price)
+        sell_qty = int(sell_qty)
 
-    if not available_holdings.exists():
-        return Response({"detail": "You don't have any holdings for the requested stock"}, status=status.HTTP_400_BAD_REQUEST)
+        price_diff = ((sell_price-stock.current_price)/(stock.current_price))*100
+        if price_diff > 2 or price_diff < -2:
+            return Response({"detail": "Cannot sell at this price"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        available_holdings = Holding.objects.filter(portfolio=portfolio, stock=stock).order_by("transaction__transaction_datetime")
 
-    available_qty = (available_holdings.aggregate(total_qty=Sum("quantity"))).get("total_qty", 0)
+        if not available_holdings.exists():
+            return Response({"detail": "You don't have any holdings for the requested stock"}, status=status.HTTP_400_BAD_REQUEST)
 
-    if sell_qty>available_qty:
-        return Response({"detail": "You don't have enough available stocks"}, status=status.HTTP_400_BAD_REQUEST)
+        available_qty = (available_holdings.aggregate(total_qty=Sum("quantity"))).get("total_qty", 0)
 
-    for holding in available_holdings:
-        if sell_qty==0:
-            break
-        diff = min(holding.quantity, sell_qty)
-        holding.quantity -= diff
-        sell_qty -= diff
-        holding.save()
-        if holding.quantity == 0:
-            holding.delete()
+        if sell_qty>available_qty:
+            return Response({"detail": "You don't have enough available stocks"}, status=status.HTTP_400_BAD_REQUEST)
 
-    portfolio.cash += (sell_qty * sell_price)
-    portfolio.save
+        for holding in available_holdings:
+            if sell_qty==0:
+                break
+            diff = min(holding.quantity, sell_qty)
+            holding.quantity -= diff
+            sell_qty -= diff
+            holding.save()
+            if holding.quantity == 0:
+                holding.delete()
 
-    transaction = Transaction.objects.create(
-        user=user,
-        stock=stock,
-        traded_price=sell_price,
-        quantity=sell_qty,
-        transaction_type="sell"
-    )
+        portfolio.cash += (sell_qty * sell_price)
+        portfolio.save
 
-    return Response({"detail": "Transaction completed"}, status=status.HTTP_201_CREATED)
+        transaction = Transaction.objects.create(
+            user=user,
+            stock=stock,
+            traded_price=sell_price,
+            quantity=sell_qty,
+            transaction_type="sell"
+        )
+
+        return Response({"detail": "Transaction completed"}, status=status.HTTP_201_CREATED)
 
 
 class PortfolioApi(APIView):
 
     def get(self, request):
+
         user = request.user
         portfolio = Portfolio.objects.get(user=user)
         portfolio_serializer = serializers.PortfolioSerializer(portfolio)
+
         return Response(portfolio_serializer.data, status=status.HTTP_200_OK)
